abb49a90c46dce1e64103a959377d6e7994c2477
Aaron Lengyel <aaron.lengyel@ucalgary.ca>
2021-11-13 20:59:12 -0700

    Fixed gitignore and TOKEN

--- telegram-bot/main.py
+++ telegram-bot/main.py
@@ -6,8 +6,9 @@ def hello(update: Update, context: CallbackContext) -> None:
     update.message.reply_text(f'Hello {update.effective_user.first_name}')
 
 with open('TOKEN.txt','r') as f:
+    TOKEN = f.read().strip()
 
-    updater = Updater(f.read())
+    updater = Updater(TOKEN)
 
     updater.dispatcher.add_handler(CommandHandler('hello', hello))
 
