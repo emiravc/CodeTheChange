20ad0199c42ca6e41ee2b32c946b77bd4175ee28
Aaron Lengyel <aaron.lengyel@ucalgary.ca>
2021-11-13 20:44:49 -0700

    Starting fresh

---
+++ telegram-bot/main.py
@@ -0,0 +1,15 @@
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, CallbackContext
+
+
+def hello(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text(f'Hello {update.effective_user.first_name}')
+
+with open('TOKEN.txt','r') as f:
+
+    updater = Updater(f.read())
+
+    updater.dispatcher.add_handler(CommandHandler('hello', hello))
+
+    updater.start_polling()
+    updater.idle()
